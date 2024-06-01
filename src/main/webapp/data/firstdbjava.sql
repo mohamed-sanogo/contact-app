72b42c172b2cf4095f2e0d4f00147fd0aa2376f7
mohamed-sanogo <euromoh68@gmail.com>
2024-06-01 16:46:13 +0000

    db

--- src/main/webapp/data/firstdbjava.sql
+++ src/main/webapp/data/firstdbjava.sql
@@ -32,7 +32,8 @@ CREATE TABLE `contact` (
   `nom` varchar(50) NOT NULL,
   `tel` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `adresse` varchar(50) NOT NULL
+  `adresse` varchar(50) NOT NULL,
+  `competence` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
